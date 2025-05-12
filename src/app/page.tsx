630c025509cb821966cb12b8e2a7af90d0b6a881
Anaru Herbert <anaruherbert@gmail.com>
2025-05-12 23:38:25 +1000

    fix mock data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,46 @@
 import { ReviewsList, type UserReview } from "~/components/ReviewsList";
 
-export const mockData: UserReview[] = [
-  {
-    id: "123456",
-    reviewedBy: {
-      userId: "1",
-      name: "Anaru",
-    },
-    reviewDate: "2025-05-10T17:00:00",
-    notes: "",
-    status: "uploaded",
-    previewImage:
-      "https://3gyi2yzoja.ufs.sh/f/6MkX0sloZNbBe0j1rjWqSa8VGxmtUODwrgP2NqIvE74kFLpn",
-  },
-
-  {
-    id: "456",
-    reviewedBy: {
-      userId: "1",
-      name: "Anaru",
-    },
-    reviewDate: "2025-05-11T17:00:00",
-    notes: "",
-    status: "assigned",
-    previewImage:
-      "https://3gyi2yzoja.ufs.sh/f/6MkX0sloZNbBe0j1rjWqSa8VGxmtUODwrgP2NqIvE74kFLpn",
-  },
-  {
-    id: "789",
-    reviewedBy: {
-      userId: "1",
-      name: "Anaru",
-    },
-    notes:
-      "Need better timing and active footwork after clear so you are ready for the next shot",
-    status: "reviewed",
-    reviewDate: "2025-05-12T17:00:00",
-    previewImage:
-      "https://3gyi2yzoja.ufs.sh/f/6MkX0sloZNbBe0j1rjWqSa8VGxmtUODwrgP2NqIvE74kFLpn",
-  },
-];
-
 export default function HomePage() {
+  const mockData: UserReview[] = [
+    {
+      id: "123456",
+      reviewedBy: {
+        userId: "1",
+        name: "Anaru",
+      },
+      reviewDate: "2025-05-10T17:00:00",
+      notes: "",
+      status: "uploaded",
+      previewImage:
+        "https://3gyi2yzoja.ufs.sh/f/6MkX0sloZNbBe0j1rjWqSa8VGxmtUODwrgP2NqIvE74kFLpn",
+    },
+
+    {
+      id: "456",
+      reviewedBy: {
+        userId: "1",
+        name: "Anaru",
+      },
+      reviewDate: "2025-05-11T17:00:00",
+      notes: "",
+      status: "assigned",
+      previewImage:
+        "https://3gyi2yzoja.ufs.sh/f/6MkX0sloZNbBe0j1rjWqSa8VGxmtUODwrgP2NqIvE74kFLpn",
+    },
+    {
+      id: "789",
+      reviewedBy: {
+        userId: "1",
+        name: "Anaru",
+      },
+      notes:
+        "Need better timing and active footwork after clear so you are ready for the next shot",
+      status: "reviewed",
+      reviewDate: "2025-05-12T17:00:00",
+      previewImage:
+        "https://3gyi2yzoja.ufs.sh/f/6MkX0sloZNbBe0j1rjWqSa8VGxmtUODwrgP2NqIvE74kFLpn",
+    },
+  ];
   return (
     <main className="container mx-auto py-8">
       <h1 className="mb-8 text-center text-3xl font-bold">
