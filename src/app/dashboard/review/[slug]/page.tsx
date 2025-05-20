94365c6799710fd939c9b142f6b11dd82db7b23a
Anaru Herbert <anaruherbert@gmail.com>
2025-05-20 17:12:24 +1000

    add some good defaults for bg and text colour

--- src/app/dashboard/review/[slug]/page.tsx
+++ src/app/dashboard/review/[slug]/page.tsx
@@ -5,8 +5,10 @@ export default function Page() {
   const params = useParams();
   const { slug } = params;
   return (
-    <main className="container mx-auto py-8">
-      <h1 className="mb-8 text-center text-3xl font-bold">Slug : {slug}</h1>
+    <main className="container mx-auto pt-24 pb-8">
+      <h1 className="text-primary-foreground mb-8 text-center text-3xl font-bold">
+        Slug : {slug}
+      </h1>
     </main>
   );
 }
