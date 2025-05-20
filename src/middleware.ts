e119aa49e8b944b1aeb687f5c9a3198b2f7d9da3
Anaru Herbert <anaruherbert@gmail.com>
2025-05-20 15:18:09 +1000

    remove middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,5 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/"]);
-export default clerkMiddleware(async (auth, req) => {
-  if (!isPublicRoute(req)) await auth.protect();
-});
-
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
