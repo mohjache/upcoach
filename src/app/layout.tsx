7b13ffae0d9ba42ea68cf1103c374cc1cd6441e5
Anaru Herbert <anaruherbert@gmail.com>
2025-05-22 10:12:51 +1000

    fix pathing for after signout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${geist.variable}`}>
       <body>
-        <ClerkProvider afterSignOutUrl={"/"}>
+        <ClerkProvider afterSignOutUrl={"./"}>
           <ConvexClientProvider>
             <TopNav />
             {children}
