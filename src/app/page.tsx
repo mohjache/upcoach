b4e8b4716e97469b7821836b90518225693c45b9
Anaru Herbert <anaruherbert@gmail.com>
2025-05-22 10:07:32 +1000

    dont add catch to redirect from navigation if person is authenticated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,7 @@
 import { SignedOut, SignInButton } from "@clerk/nextjs";
-import { auth, currentUser } from "@clerk/nextjs/server";
-import { redirect } from "next/navigation";
 import { Button } from "~/components/ui/button";
 
 export default async function HomePage() {
-  const { userId } = await auth();
-
-  if (userId) {
-    redirect("/dashboard");
-  }
   return (
     <>
       <div className="bg-primary text-primary-foreground flex min-h-screen flex-col items-center justify-center">
