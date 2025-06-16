7e5be6152b5fe52af30d5465203d33c3e674d989
Anaru Herbert <anaruherbert@gmail.com>
2025-06-16 15:57:15 +1000

    Remove suspense boundary

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -1,14 +1,10 @@
 "use client";
 import { CreateOrganization } from "@clerk/nextjs";
-import { Suspense } from "react";
-import { FallbackComponent } from "~/components/Fallback";
 
 export default function Page() {
   return (
     <div className="flex h-screen flex-col items-center justify-center overflow-hidden">
-      <Suspense fallback={<FallbackComponent />}>
-        <CreateOrganization afterCreateOrganizationUrl="/dashboard" />
-      </Suspense>
+      <CreateOrganization afterCreateOrganizationUrl="/dashboard" />
     </div>
   );
 }
