ff9738fc3bde30b5f48d3d4ddd456e647a45f48d
Anaru Herbert <anaruherbert@gmail.com>
2025-06-16 21:34:10 +1000

    Add menu item to manage orgs

--- src/components/Navigation/TopNav.tsx
+++ src/components/Navigation/TopNav.tsx
@@ -1,8 +1,21 @@
 "use client";
-import { UserButton, SignInButton } from "@clerk/nextjs";
+import { UserButton, SignInButton, OrganizationProfile } from "@clerk/nextjs";
 import { Button } from "../ui/button";
 import Link from "next/link";
 import { Authenticated, Unauthenticated } from "convex/react";
+import { Building } from "lucide-react";
+
+const DotIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 512 512"
+      fill="currentColor"
+    >
+      <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512z" />
+    </svg>
+  );
+};
 
 export const TopNav = () => {
   return (
@@ -14,7 +27,15 @@ export const TopNav = () => {
       </div>
       <div className="flex items-center justify-center">
         <Authenticated>
-          <UserButton />
+          <UserButton>
+            <UserButton.MenuItems>
+              <UserButton.Link
+                label="Manage Organisation"
+                labelIcon={<Building size={16} />}
+                href="/dashboard/manage-organisation"
+              />
+            </UserButton.MenuItems>
+          </UserButton>
         </Authenticated>
         <Unauthenticated>
           <SignInButton>
