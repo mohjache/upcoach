29d272a7071d14d5d97acbda34212caab4c5e21b
Anaru Herbert <anaruherbert@gmail.com>
2025-10-19 18:40:06 +1000

    Add more padding to bottom

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -10,7 +10,7 @@ const Page = () => {
       <NavBar />
       <h1 className="p-4 px-8 text-4xl font-extrabold">Welcome to UpCoach</h1>
       <AuthLoading>
-        <div className="grid grid-cols-1 gap-4 px-8 pb-4 lg:grid-cols-2 xl:grid-cols-3">
+        <div className="grid grid-cols-1 gap-4 px-8 pb-32 lg:grid-cols-2 xl:grid-cols-3">
           <Skeleton className="h-96 w-full flex-none" />
           <Skeleton className="h-96 w-full flex-none" />
           <Skeleton className="h-96 w-full flex-none" />
@@ -20,14 +20,15 @@ const Page = () => {
         </div>
       </AuthLoading>
       <Authenticated>
-        <div className="grid grid-cols-1 gap-4 px-8 pb-8 lg:grid-cols-2 xl:grid-cols-3">
+        <div className="grid grid-cols-1 gap-4 px-8 pb-32 lg:grid-cols-2 xl:grid-cols-3">
           <div className="h-96 w-full flex-none bg-amber-600" />
           <div className="h-96 w-full flex-none bg-amber-600" />
+          {/* <div className="h-96 w-full flex-none bg-amber-600" />
           <div className="h-96 w-full flex-none bg-amber-600" />
           <div className="h-96 w-full flex-none bg-cyan-700" />
           <div className="h-96 w-full flex-none bg-cyan-700" />
           <div className="h-96 w-full flex-none bg-cyan-700" />
-          <div className="h-96 w-full flex-none bg-emerald-600" />
+          <div className="h-96 w-full flex-none bg-emerald-600" /> */}
         </div>
       </Authenticated>
     </>
